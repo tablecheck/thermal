7e0da5e75ac3f62595bad52285aeb2fd39126801
johnnyshields <27655+johnnyshields@users.noreply.github.com>
2025-03-12 18:13:44 +0900

    Fix specs

--- spec/thermal/thermal_spec.rb
+++ spec/thermal/thermal_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Thermal do
   end
 
   let(:stub_rails) do
-    rails = class_double('Rails')
+    rails = double('Rails') # rubocop:disable RSpec/VerifiedDoubles
     allow(rails).to receive(:root).and_return(Pathname.new('/a/b/c'))
     stub_const('Rails', rails)
   end
